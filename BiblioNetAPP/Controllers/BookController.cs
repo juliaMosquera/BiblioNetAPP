eb7f930d6b3e9e6fa5338790b839a99a0d4464ff
juliaMosquera <julia.llarisa@gmail.com>
2022-03-22 12:10:09 -0500

    Register Book

--- BiblioNetAPP/Controllers/BookController.cs
+++ BiblioNetAPP/Controllers/BookController.cs
@@ -1,10 +1,17 @@
 ﻿using BiblioNetAPP.Models;
+using BiblioNetAPP.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BiblioNetAPP.Controllers
 {
     public class BookController :Controller
     {
+        private readonly IRepositorieBook repositorieBook;
+        public BookController(IRepositorieBook repositorieBook)
+        {
+            // this referencia a las variables locales del archivo
+            this.repositorieBook = repositorieBook;
+        }
         public IActionResult Create()
         {
             return View();
@@ -17,6 +24,11 @@ namespace BiblioNetAPP.Controllers
             {
                 return View(book);
             }
+
+            book.IdAuthor = 1;
+
+            repositorieBook.Create(book);
+
             return View();
         }
     }
---
+++ BiblioNetAPP/Services/RepositorieBook.cs
@@ -0,0 +1,33 @@
+﻿using BiblioNetAPP.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BiblioNetAPP.Services
+{
+
+    public interface IRepositorieBook
+    {
+        void Create(Book book);
+    }
+    public class RepositorieBook : IRepositorieBook
+    {
+        private readonly string connectionString;
+
+        public RepositorieBook(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void Create(Book book)
+        {
+
+            //Microsoft.Data.SqlClient;
+            using var connection = new SqlConnection(connectionString);
+            //Dapper
+            var id = connection.QuerySingle<int>($@"Insert into Book
+                                                (BookName, IdAuthor, Price) 
+                                                VALUES (@BookName, @IdAuthor, @Price); SELECT SCOPE_IDENTITY();",book);
+            book.Id = id;
+        }
+    }
+}
